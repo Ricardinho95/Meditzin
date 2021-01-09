49e1da822da84d8bb2092dd4e8d814fe0d597a2d
Daniel <kenzoboo@hotmail.com>
2021-01-09 17:57:03 -0600

    Segundo commit, Mejoras en la interfaz principal

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAWX5pa56-TjoWYR_aeHsnVRVnfrnNg9ss",
+    authDomain: "meditzin.firebaseapp.com",
+    projectId: "meditzin",
+    storageBucket: "meditzin.appspot.com",
+    messagingSenderId: "187878313361",
+    appId: "1:187878313361:web:9f20e7e62706415370de30"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
